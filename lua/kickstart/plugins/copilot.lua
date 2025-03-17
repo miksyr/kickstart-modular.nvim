d29a66068a0a44873674795e45f1d268ab0d443b
Mike Doran <mikrdoran@gmail.com>
2025-03-17 13:14:02 +0000

    .

--- lua/kickstart/plugins/copilot.lua
+++ lua/kickstart/plugins/copilot.lua
@@ -5,13 +5,10 @@ return {
 		cmd = "Copilot",
 		event = "InsertEnter",
 		opts = {
-			-- I don't find the panel useful.
-			panel = { enabled = true },
+			panel = { enabled = false },
 			suggestion = {
 				auto_trigger = true,
-				-- Use alt to interact with Copilot.
 				keymap = {
-					-- Disable the built-in mapping, we'll configure it in nvim-cmp.
 					accept = false,
 					accept_word = "<M-w>",
 					accept_line = "<C-y>",
